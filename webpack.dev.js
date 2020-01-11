8f78e46ec45d515394b362e908145974307aef0f
Hata <hato6502@gmail.com>
2020-01-11 22:36:49 +0900

    Install WebPack for dist

---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line import/no-extraneous-dependencies
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  entry: './src/dev/index.tsx',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: 'public',
+    hot: true,
+    open: true
+  }
+});
